8b0c0b0e989ffd215ce7eea6857a31f78464efa4
jaydeep-makwana <jaydeepmakwna570@gmail.com>
2022-04-05 21:32:33 +0530

    test

--- laravel.php
+++ laravel.php
@@ -9,49 +9,55 @@ $total_amount = $money + $money * $interest * $years * 12 / 100;
 
 // echo $total_amount;
 
-
-# mobile details 
-$phone = "apple";
-
 $content = file_get_contents('json_data.php');
 $json = json_decode($content, true);
 
-function details($index)
+
+
+
+function jm($phone)
 {
-    function data($index, $key)
+
+
+    function details($index)
     {
-        global $json;
-        echo $json[$index][$key];
-    }
+        function data($index, $key)
+        {
+            global $json;
+            echo $json[$index][$key];
+        }
 ?>
 
-    <table border>
-        <tr>
-            <th>Model</th>
-            <td><?php data($index, 'modalName'); ?></td>
-        </tr>
-        <tr>
-            <th>Price</th>
-            <td><?php data($index, 'price'); ?></td>
+        <table border>
+            <tr>
+                <th>Model</th>
+                <td><?php data($index, 'modalName'); ?></td>
+            </tr>
+            <tr>
+                <th>Price</th>
+                <td><?php data($index, 'price'); ?></td>
 
-        </tr>
-    </table>
+            </tr>
+        </table>
 <?php
+    }
+
+
+    switch ($phone) {
+        case 'vivo':
+            details(0);
+            break;
+        case 'mi':
+            details(1);
+            break;
+        case 'apple':
+            details(2);
+            break;
+
+        default:
+            echo 'this modal is not available in our list';
+            break;
+    }
 }
 
-
-switch ($phone) {
-    case 'vivo':
-        details(0);
-        break;
-    case 'mi':
-        details(1);
-        break;
-    case 'apple':
-        details(2);
-        break;
-
-    default:
-        echo 'this modal is not available in our list';
-        break;
-}
+jm('apple');
