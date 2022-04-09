6e8d458e34c2cf5f11dafc931a5169b035290071
jaydeep-makwana <jaydeepmakwana570@gmail.com>
2022-04-09 19:31:17 +0530

    test

--- test.php
+++ test.php
@@ -1,16 +1,28 @@
 <?php
  
+ $conn = mysqli_connect('localhost','root','','mydb');
+
+ 
+ 
+     
  
 
 // echo $a ;
  
+$query = "SELECT * FRO user";
+
+$result = mysqli_query($conn, $query);
+ 
+// echo mysqli_errno($conn);
 
 
+$assoc = mysqli_fetch_assoc($result);
 
-
+echo mysqli_error($conn);
+// echo mysqli_errno($conn);
 
 
 echo '<pre>';
 // var_dump($a);
-// print_r($a);
+print_r($assoc);
 echo '</pre>';
