ef0c19623640a0dbe09a2f44f9e5666a3f2ddb7d
jaydeep-makwana <jaydeepmakwana570@gmail.com>
2022-05-14 12:59:58 +0530

    dsa

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$array = ['name'=>'jay','age'=>22 ];
+$a = 123;
+// echo is_array($a); # return 1 if success else nothing
+// echo in_array(22,$array); # return 1 if success else nothing
+
+
+
+ 
+
+
+
+
+?>
